4ec57cdc4c2cd8500ae70e0e4c25428b8aebdaf3
R5Z Yoon <asaucybloom@gmail.com>
2023-07-14 18:10:49 +0900

    API for querying job list and selected job

--- servers/be-server/app.py
+++ servers/be-server/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 from models import db, Job, Image
 
+import os
+import openai
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///job.db'
 db.init_app(app)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 
 @app.route('/')
@@ -16,20 +21,37 @@ def hello():
 def get_jobs():
     jobs = Job.query.all()
 
-    job_list = []
+    jobList = []
     for job in jobs:
         images = Image.query.filter_by(job_id=job.id).all()
         images_list = [image.path for image in images]
         job_data = {
-            'id': job.id,
-            'name': job.name,
-            'type': job.type,
+            'jobId': job.id,
+            'jobName': job.name,
+            'jobType': job.type,
             'images': images_list
         }
-        job_list.append(job_data)
+        jobList.append(job_data)
     
-    return jsonify(job_list)
+    return jsonify(jobList)
 
+
+# 특정 직업 조회
+@app.route('/job_list/<int:job_id>', methods=['GET'])
+def get_job(job_id):
+    job = Job.query.get(job_id)
+    if job:
+        images = Image.query.filter_by(job_id=job.id).all()
+        images_list = [image.path for image in images]
+        job_data = {
+            'jobId': job.id,
+            'jobName': job.name,
+            'jobType': job.type,
+            'images': images_list
+        }
+        return jsonify(job_data)
+    else:
+        return jsonify({'error': 'Job not found.'}), 404
 
 
 if __name__ == '__main__':
