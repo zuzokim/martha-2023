dbcb84debbecc3470c25186217f7b40cb95bd323
zuzokim <zuzokim@gmail.com>
2023-07-19 23:57:09 +0900

    allow POST method on jobselection

--- servers/be-server/app.py
+++ servers/be-server/app.py
@@ -46,7 +46,7 @@ def get_jobs():
 
 
 # 특정 직업 조회
-@app.route('/job_list/<int:job_id>', methods=['GET'])
+@app.route('/job_list/<int:job_id>', methods=['POST'])
 def get_job(job_id):
     job = Job.query.get(job_id)
     if job:
