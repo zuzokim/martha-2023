e1b0276d87a106864317b918bc7a46c4f261127b
zuzokim <zuzokim@gmail.com>
2023-09-03 00:54:25 +0900

    add postcss-nesting plugin for css nesting not supported devices

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import postcssNesting from "postcss-nesting";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  css: {
+    postcss: { plugins: [postcssNesting] },
+  },
 });
